efd2ccd132ee4dae068a1ad24f6a4ee1f16d3da3
mahadi570 <mahadihasan18407@gmail.com>
2021-07-06 23:25:42 +0600

    componet added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData'
+import './Shop.css'
+import procuct from '../product/procuct';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,15);
     const [products] = useState(first10);
     return (
         
-            <div>
-                <h1>This is mine Website</h1>
-                <h2>{products.length}</h2>
-        <ul>
-            {
-            products.map(pd => <li>{pd.name}</li>)
-            }
-            
-        </ul>
+                <div className="shop-container">
+                    <div className="product-container"><h1>This mine Website</h1>
+            <Product></Product>
+            </div>
+                <div className="cart-container">
+                    <h3>This is cart.</h3>
+                </div>
+                
             </div>
     );
 };
